287900ecfabc26740c0fc694b06b6a946224dd92
Paulius Zelvys <paulius.zelvys@ktu.edu>
2024-09-12 20:22:42 +0300

    no message

---
+++ SharedLibs/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SharedLibs
+{
+    public enum Direction
+    {
+        None,
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    public class PacmanMovement
+    {
+        public string PlayerId { get; set; }
+        public Direction Direction { get; set; }
+    }
+    public class PacmanPositionUpdate
+    {
+        public string PlayerId { get; set; }
+        public int X;
+        public int Y;
+    }
+    public class PelletCollected
+    {
+        public string PlayerId { get; set;}
+        public int X; 
+        public int Y;
+    }
+    public class PacmanDeafeated
+    {
+        public string PlayerId { get; set; }
+    }
+    public enum TileStatus
+    {
+        Wall,
+        Empty,
+        Pellet,
+        Ghost,
+        PelletAndGhost,
+        Pacman
+    }
+    public class Positions
+    {
+        public TileStatus[,] Grid { get; set; }
+    }
+}
