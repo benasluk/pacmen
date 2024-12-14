37710c1852652d2b5bbf2947cd77a38e747c6510
Benas <ben.lukosius@gmail.com>
2024-12-15 00:23:48 +0200

    Updated state so it has a separate class

--- Server/Classes/GameLogic/GameLoop.cs
+++ Server/Classes/GameLogic/GameLoop.cs
@@ -5,6 +5,7 @@ using Server.Classes.Services;
 using Server.Classes.Services.Factory;
 using Server.Classes.Services.Logging;
 using Server.Classes.Services.Observer;
+using Server.Classes.Services.State;
 using Server.GameWorld;
 using Server.Hubs;
 using SharedLibs;
@@ -31,6 +32,7 @@ namespace Server.Classes.GameLogic
         public event LevelRestart LevelRestartEvent;
         private bool levelRestarted = false;
         private readonly CommandHandler _commandHandler;
+        private StateHandler stateHandler;
 
         private AbstractLevelFactory _levelFactory;
         private List<Item> ItemList;
@@ -38,12 +40,6 @@ namespace Server.Classes.GameLogic
         private int gameSpeed = 1000 / 10;
         private Ilogger textLogger;
         private Ilogger databaseLogger;
-
-        // 0 - Not started
-        // 1 - Playing
-        // 2 - Paused
-        // 3 - Finished
-        private int State = 0;
 
         public GameLoop(GameService gameService, PlayerService playerService, MessageService messageService, IHubContext<GameHub> hubContext, GhostService ghostService, CommandHandler commandHandler)
         {
@@ -62,7 +58,7 @@ namespace Server.Classes.GameLogic
         }
         public void Start()
         {
-            State = 1;
+            stateHandler.SetState(1);
             _timer = new Timer(Update, null, 0, gameSpeed);
 
             int whatLevel = 0; // new Random(DateTime.Now.Millisecond).Next() % 2;
@@ -76,7 +72,7 @@ namespace Server.Classes.GameLogic
         }
         public void RestartLoop()
         {
-            State = 1;
+            stateHandler.SetState(1);
             ItemList = new List<Item>();
             int level = _messageService.GetLevel();
             if (level % 2 == 0) _levelFactory = new LevelOneFactory();
@@ -91,8 +87,8 @@ namespace Server.Classes.GameLogic
         }
         public async void Update(object state)
         {
-            State = _commandHandler.HandleMessages(State);
-            if(State == 1) {
+            stateHandler.SetState(_commandHandler.HandleMessages(stateHandler.GetState()));
+            if(stateHandler.GetState() == 1) {
                 if (_playerService.GetPlayerCount() >= 1)
                 {
                     _movementTimerService.UpdateElapsedTime(gameSpeed);
@@ -147,7 +143,7 @@ namespace Server.Classes.GameLogic
 
                     if(_gameService.IsMapFinished())
                     {
-                        State = 3;
+                        stateHandler.SetState(3);
                     }
                 }
             }
---
+++ Server/Classes/Services/State/StateHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace Server.Classes.Services.State
+{
+    public class StateHandler
+    {
+        // 0 - Not started
+        // 1 - Playing
+        // 2 - Paused
+        // 3 - Finished
+        private int state;
+        public StateHandler(int state)
+        {
+            this.state = state;
+        }
+
+        public StateHandler()
+        {
+            state = 0;
+        }
+
+        public void SetState(int state)
+        {
+            if (state > 4 || state < 0) return;
+            this.state = state;
+        }
+
+        public int GetState()
+        {
+            return state;
+        }
+    }
+}
